4d4c847212d3f83b2c19e787b8016f8571001c89
rajkapadia247 <47295446+rajkapadia247@users.noreply.github.com>
2020-04-26 19:22:57 +0000

    Deploying to gh-pages from  @ d33021fe289f2639476ed0f873d5a6e80e13b27c 🚀

---
+++ precache-manifest.fc17df6b6f4a37e8ade0eea68e09e9f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e42f6ad661d59b007404723c96077f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7515ce65a7d97b0925ea",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "87f457b18ae410d7c760",
+    "url": "/static/css/main.1646608c.chunk.css"
+  },
+  {
+    "revision": "7515ce65a7d97b0925ea",
+    "url": "/static/js/2.92569a40.chunk.js"
+  },
+  {
+    "revision": "577b7a3b9fbfa17fccf2f4ddb5a84180",
+    "url": "/static/js/2.92569a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f457b18ae410d7c760",
+    "url": "/static/js/main.f8aacb16.chunk.js"
+  },
+  {
+    "revision": "20e74bb56b5aad9a2c37",
+    "url": "/static/js/runtime-main.96a74c67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53f6beed6de71d7cf35199192dbde789.js"
+  "/precache-manifest.fc17df6b6f4a37e8ade0eea68e09e9f6.js"
 );
 
 self.addEventListener('message', (event) => {
